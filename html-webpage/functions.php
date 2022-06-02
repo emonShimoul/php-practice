0034241056ddd6b1b573871ec561c8afc1a71d1d
emonShimoul <emonshimoul@gmail.com>
2022-06-02 12:42:07 +0600

    added php ischecked function

---
+++ html-webpage/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function isChecked($inputName, $value){
+    if(isset($_POST[$inputName]) && is_array($_POST[$inputName]) && in_array($value, $_POST[$inputName])){
+        echo "Checked";
+    }
+}
