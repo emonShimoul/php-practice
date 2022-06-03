a2b759785143f5263c4396c1fa2e6e6dd016c29a
emonShimoul <emonshimoul@gmail.com>
2022-06-03 15:42:09 +0600

    added titles

--- html-webpage/form2.php
+++ html-webpage/form2.php
@@ -9,7 +9,7 @@ $fruits = ["mango", "apple", "Banana", "orange", "grape"];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Input Select</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
