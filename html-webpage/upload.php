26136c8e8679f6908995cbf1852086352e605741
emonShimoul <emonshimoul@gmail.com>
2022-06-09 16:57:15 +0600

    added upload file functionality

--- html-webpage/upload.php
+++ html-webpage/upload.php
@@ -1,10 +1,16 @@
+<?php
+if($_FILES['photo']){
+    move_uploaded_file($_FILES['photo']['tmp_name'], "files/image.png");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload File</title>
+    <title>Hello World</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
@@ -21,12 +27,21 @@
             <div class="column column-60 column-offset-20">
                 <h1>Welcome</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit cum soluta ex eveniet vel autem ipsam dicta officia illo est?</p>
+
+                <pre>
+                <p>
+                    <?php
+                    print_r($_POST);
+                    print_r($_FILES);
+                    ?>
+                </p>
+            </pre>
             </div>
         </div>
 
         <div class="row">
         <div class="column column-60 column-offset-20">
-            <form method="POST">
+            <form method="POST" enctype="multipart/form-data">
                 <label for="fname">First Name</label>
                 <input type="text" name="fname" id="fname">
 
